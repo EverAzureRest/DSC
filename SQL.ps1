f1e02f5490dc8f3cb0b45276ec8c2210ddac714b
lorax79 <jordan.smith@powerhell.org>
2016-09-07 13:03:40 -0700

    Add files via upload

---
+++ SQL.ps1
@@ -0,0 +1,47 @@
+﻿Configuration AppSQL
+{
+import-dscresource -ModuleName xSQLServer
+$setupcred = get-automationpscredential -Name SQLSetupAcct
+$instancename = "MSSQLSERVER"
+$DBSvcAcct = get-automationpscredential -Name SQLSvcAcct
+$sacred = get-automationpscredential -Name SAAcct
+$sasecurepwd = $sacred.Password
+$sapwd = $sacred.GetNetworkCredential().Password
+
+Node DBE
+{
+
+InstanceName = $instancename
+SetupCredential = $setupcred
+InstallSharedDir = "C:\Program Files\Microsoft SQL Server\"
+InstanceDir = "C:\Program Files\Microsoft SQL Server\$($intancename)\"
+InstallSQLDataDir = "E:\Databases\"
+SourcePath = "\\Path\to\Network\Share\SQLinstaller.msi"
+SecurityMode = "Mixed"
+SQLSvcAccount = $DBSvcAcct
+SQLTempDBDir = "F:\DBTEMP\"
+SQLTempDBLogDir = "G:\DBTEMPLOGS\"
+UpdateEnabled = $true
+SAPWD = $sapwd
+
+}
+
+Node RS
+{
+InstanceName = $instancename
+RSSQLInstanceName = "ReportingServices"
+SetupCredential = $setupcred
+InstallSharedDir = "C:\Program Files\Microsoft SQL Server\"
+InstanceDir = "C:\Program Files\Microsoft SQL Server\$($intancename)\"
+InstallSQLDataDir = "E:\Databases\"
+SourcePath = "\\Path\to\Network\Share\SQLinstaller.msi"
+SecurityMode = "Mixed"
+SQLSvcAccount = $DBSvcAcct
+Features = "Reporting Sevices"
+
+}
+
+
+
+
+}
