58c9ca2209ac403c9b71acbeb6bda12abcedd555
lorax79 <jordan.smith@powerhell.org>
2016-12-08 11:20:37 -0800

    use dsc config to compile

---
+++ SQL-DSCconfig.ps1
@@ -0,0 +1,14 @@
+﻿$ConfigData = @{
+    AllNodes = @(
+        @{
+            NodeName = "*"
+            PSDscAllowPlainTextPassword = $True
+        },
+                @{
+            NodeName = "DBE"
+            PSDscAllowDomainUser = $true
+        }
+    )
+}
+
+Start-AzureRmAutomationDscCompilationJob -ResourceGroupName "eus2services" -AutomationAccountName "labsubautomation" -ConfigurationName "SQL" -ConfigurationData $ConfigData
